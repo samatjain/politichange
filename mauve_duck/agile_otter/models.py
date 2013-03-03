0a0dbfe06514d4a5c6088078de469826d4f42d63
Ronnie Garduño <koko@rpg-free.com>
2013-03-02 21:23:13 -0700

    allowed database fields to be null as well; this was different from letting forms be blank(!)

--- mauve_duck/agile_otter/models.py
+++ mauve_duck/agile_otter/models.py
@@ -7,16 +7,16 @@ class Link(models.Model):
 
 class Campaign(models.Model):
 	position				= models.CharField(max_length=255)
-	fiveStances 		= models.ForeignKey('Stance', blank=True)
-	first100Days		= models.TextField(blank=True)
-	whyBestCandidate	= models.TextField(blank=True)
-	videoURL				= models.CharField(max_length=255,blank=True)
+	fiveStances 		= models.ForeignKey('Stance', blank=True,null=True)
+	first100Days		= models.TextField(blank=True,null=True)
+	whyBestCandidate	= models.TextField(blank=True,null=True)
+	videoURL				= models.CharField(max_length=255,blank=True,null=True)
 	fundingThreshold	= models.DecimalField(max_digits=8, decimal_places=0)
 	campaignEndDate	= models.DateField()
 	electionEndDate	= models.DateField()
-	amountPledged		= models.DecimalField(max_digits=8, decimal_places=0,blank=True)
-	numberSupporters  = models.DecimalField(max_digits=8, decimal_places=0,blank=True)
-	links					= models.ForeignKey(Link,blank=True)
+	amountPledged		= models.DecimalField(max_digits=8, decimal_places=0,blank=True,null=True)
+	numberSupporters  = models.DecimalField(max_digits=8, decimal_places=0,blank=True,null=True)
+	links					= models.ForeignKey(Link,blank=True,null=True)
 
 class Stance(models.Model):
 	text	= models.TextField()
@@ -26,9 +26,9 @@ class Participant(models.Model):
 	address 		= models.CharField(max_length=255)
 	email	  		= models.EmailField(max_length=255)
 	bio     		= models.TextField()
-	philosophy 	= models.TextField(blank=True)
+	philosophy 	= models.TextField(blank=True,null=True)
 	goals			= models.CharField(max_length=255)
-	links			= models.ForeignKey(Link,blank=True)
+	links			= models.ForeignKey(Link,blank=True,null=True)
 
 models = [Campaign, Link, Stance, Participant]
 
