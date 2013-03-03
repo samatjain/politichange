8908f7577ba236776ecadf0a4e64efbdd49832d5
Ronnie Garduño <koko@rpg-free.com>
2013-03-02 20:23:04 -0700

    put user and foreign keys back

--- agile_otter/models.py
+++ agile_otter/models.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 class Campaign(models.Model):
 	position				= models.CharField(max_length=255)
-	# fiveStances 		= models.ForeignKey('Stance')
+	fiveStances 		= models.ForeignKey('Stance')
 	first100Days		= models.TextField()
 	whyBestCandidate	= models.TextField()
 	videoURL				= models.CharField(max_length=255)
@@ -12,7 +12,7 @@ class Campaign(models.Model):
 	electionEndDate	= models.DateField()
 	amountPledged		= models.DecimalField(max_digits=8, decimal_places=0)
 	numberSupporters  = models.DecimalField(max_digits=8, decimal_places=0)
-	# links					= models.ForeignKey('Link')
+	links					= models.ForeignKey('Link')
 
 class Link(models.Model):
 	title		=	models.CharField(max_length=255)
@@ -21,6 +21,15 @@ class Link(models.Model):
 class Stance(models.Model):
 	text	= models.TextField()
 
-models = [Campaign, Link, Stance]
+class User(models.Model):
+	name 	  		= models.CharField(max_length=255)
+	address 		= models.CharField(max_length=255)
+	email	  		= models.EmailField(max_length=255)
+	bio     		= models.TextField()
+	philosophy 	= models.TextField()
+	goals			= models.CharField(max_length=255)
+	# links			= models.ForeignKey('Link')
+
+models = [Campaign, Link, Stance, User]
 
 map(admin.site.register,models)
