3822e541c7dd4529db5b7a9e777414946876db90
Ronnie Garduño <koko@rpg-free.com>
2013-03-02 21:12:23 -0700

    Made some fields optional for ease of development testing

--- mauve_duck/agile_otter/models.py
+++ mauve_duck/agile_otter/models.py
@@ -7,16 +7,16 @@ class Link(models.Model):
 
 class Campaign(models.Model):
 	position				= models.CharField(max_length=255)
-	fiveStances 		= models.ForeignKey('Stance')
-	first100Days		= models.TextField()
-	whyBestCandidate	= models.TextField()
-	videoURL				= models.CharField(max_length=255)
+	fiveStances 		= models.ForeignKey('Stance', blank=True)
+	first100Days		= models.TextField(blank=True)
+	whyBestCandidate	= models.TextField(blank=True)
+	videoURL				= models.CharField(max_length=255,blank=True)
 	fundingThreshold	= models.DecimalField(max_digits=8, decimal_places=0)
 	campaignEndDate	= models.DateField()
 	electionEndDate	= models.DateField()
-	amountPledged		= models.DecimalField(max_digits=8, decimal_places=0)
-	numberSupporters  = models.DecimalField(max_digits=8, decimal_places=0)
-	links					= models.ForeignKey(Link)
+	amountPledged		= models.DecimalField(max_digits=8, decimal_places=0,blank=True)
+	numberSupporters  = models.DecimalField(max_digits=8, decimal_places=0,blank=True)
+	links					= models.ForeignKey(Link,blank=True)
 
 class Stance(models.Model):
 	text	= models.TextField()
@@ -26,9 +26,9 @@ class Participant(models.Model):
 	address 		= models.CharField(max_length=255)
 	email	  		= models.EmailField(max_length=255)
 	bio     		= models.TextField()
-	philosophy 	= models.TextField()
+	philosophy 	= models.TextField(blank=True)
 	goals			= models.CharField(max_length=255)
-	links			= models.ForeignKey(Link)
+	links			= models.ForeignKey(Link,blank=True)
 
 models = [Campaign, Link, Stance, Participant]
 
