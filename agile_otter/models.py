962ea4e4b56836d6efaa981ae125a8d6c6b58629
Ronnie Garduño <koko@rpg-free.com>
2013-03-02 19:29:58 -0700

    Registered user model with admin app

--- agile_otter/models.py
+++ agile_otter/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib import admin
 
 class User(models.Model):
 	name 	  = models.CharField(max_length=255)
 	address = models.CharField(max_length=255)
 	email	  = models.CharField(max_length=255)
 	
+admin.site.register(User)
